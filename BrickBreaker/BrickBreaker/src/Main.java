1514b0331669425dbf687434de5cdd48d405b6b8
juanRivasMaldonado <85120823+juanRivasMaldonado@users.noreply.github.com>
2022-06-17 12:08:33 -0700

    Add files via upload

---
+++ BrickBreaker/BrickBreaker/src/Main.java
@@ -0,0 +1,20 @@
+import javax.swing.JFrame;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		JFrame obj = new JFrame();
+		GamePlay gamePlay = new GamePlay();
+		obj.setBounds(10, 10, 700, 600);
+		obj.setTitle("Brick Breaker");
+		obj.setResizable(false);
+		obj.add(gamePlay);
+		obj.setVisible(true);
+		obj.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		obj.setLocationRelativeTo(null);
+		
+
+	}
+
+}
